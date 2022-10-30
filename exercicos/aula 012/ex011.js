e22c3cb4d808851b16e8144b91e626d0dab7aeb6
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-10-29 22:31:27 -0300

    ex012 e 013

---
+++ exercicos/aula 012/ex011.js
@@ -0,0 +1,9 @@
+var idade = 67
+if (idade < 16){
+    console.log('Não vota')
+}else{
+    if(idade < 18){
+        console.log('voto opcional')
+    }else  (idade >= 18)
+        console.log('vota')
+}
