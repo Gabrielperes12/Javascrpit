d70b194b206acae0832c6c4696d9a187cc3bb386
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-04 19:14:16 -0300

    aula15

---
+++ exercicos/aula015/ambiente.js
@@ -0,0 +1,8 @@
+var numero =[5, 8, 2, 9, 3]
+numero.sort()
+numero.push(1)
+console.log(numero)
+console.log(`o vetor tem ${numero.length}`)
+console.log(`o promeiro valor do vetor é ${numero[0]}`)
+
+ 
---
+++ exercicos/aula015/ambiente2.js
@@ -0,0 +1,6 @@
+/* exemplo 1
+var valores = [8, 1, 7, 4, 2, 9]
+for( var pos=0; pos < valores.length; pos++){
+    console.log(`a posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
